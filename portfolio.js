b818af978a530a008af7b6e47fd5fb5d719a6d46
Hien Lam <hienlam@Hiens-MacBook-Pro.local>
2019-03-19 16:44:29 -0400

    first commit

---
+++ portfolio.js
@@ -0,0 +1,124 @@
+export const portfolioJson = [
+  {
+    title: "Augmented Reality",
+    purpose:
+      "To generate web-based animated content using A-Frame, an augmented reality library which uses paper-based markers",
+    tech: "",
+    desc: "",
+    url: ""
+  },
+  {
+    title: "Webflow",
+    purpose:
+      "To generate web-based animated content and incorporate keyframes and animation paths using Webflow",
+    tech: "",
+    desc: "",
+    url: ""
+  },
+  {
+    title: "Adobe Animate",
+    purpose:
+      "To generate web-based animated content and incorporate keyframes and animation paths using Adobe Animate",
+    tech: "",
+    desc: "",
+    url: ""
+  },
+  {
+    title: "animation paths",
+    purpose:
+      "To learn how to incorporate animation along a path using Greensock",
+    tech: "",
+    desc: "",
+    url: ""
+  },
+  {
+    title: "drawSVG, splitText",
+    purpose:
+      "To learn how to use Greensock's plugins: drawSVG, splitText, Draggable",
+    tech: "",
+    desc: "",
+    url: ""
+  },
+  {
+    title: "Banner Ad",
+    purpose:
+      "To incorporate timelines and employ scripting to control and animate sprite images",
+    tech: "",
+    desc: "",
+    url: ""
+  },
+  {
+    title: "morphSVG",
+    purpose:
+      "To learn how to use Greensock's plugin called morphSVG to morph between two SVG images",
+    tech: "",
+    desc: "",
+    url: ""
+  },
+  {
+    title: "timelines",
+    purpose:
+      "To incorporate timelines and employ scripting to control and animate svg elements",
+    tech: "",
+    desc: "",
+    url: ""
+  },
+  {
+    title: " SVG Bending Line ",
+    purpose:
+      " To understand SVG path data and how TweenMax can animate between 2 different sets of them ",
+    tech:
+      " Anthony Dugois' 'SVG Path Builder' - https://codepen.io/anthonydugois/pen/mewdyZ ",
+    desc:
+      " Using the SVG Path builder, I created 2 svg paths and used the generated path coordinates which TweenMax used to animate. The result was a bending line similar to a wagging tail. ",
+    url: " https://codepen.io/avcoder/full/bZBpNV "
+  },
+  {
+    title: "Toggle",
+    purpose:
+      "To construct a popular user-interface such as the toggle that incorporates animation and to maniuplate elements based on user-intitiated events",
+    tech: "",
+    desc: "",
+    url: ""
+  },
+  {
+    title: "jQuery animation",
+    purpose:
+      "To learn and use jQuery's slide and fade effects, jQuery UI's animations, and Penner's easing plugin",
+    tech: "",
+    desc: "",
+    url: ""
+  },
+  {
+    title: "@keyframes",
+    purpose:
+      "To incorporate pure CSS keyframe animations along with transforms and transitions",
+    tech: "",
+    desc: "",
+    url: ""
+  },
+  {
+    title: "Sprite animation",
+    purpose:
+      "To emulate Twitter's Like button via sprite-based animation using keyframes and steps",
+    tech: "",
+    desc: "",
+    url: ""
+  },
+  {
+    title: "Storyboard",
+    purpose:
+      "To develop a simple storyboard to describe an animated sequence or a user interaction.",
+    tech: "",
+    desc: "",
+    url: ""
+  },
+  {
+    title: "The 12 principles",
+    purpose:
+      "To explain the basic elements and principles of animation/motion graphics in support of web-based interface design",
+    tech: "",
+    desc: "",
+    url: ""
+  }
+];
